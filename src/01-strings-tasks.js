9e3147e62cdc72cc12715e37f44a3d033f9ebab2
SAKURALIX <sakura.xlix@gmail.com>
2023-06-10 06:07:59 +0300

    feat: 19 tasks are done

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -242,8 +242,14 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  const a = value;
+  if (a === undefined || a === null) {
+    return false;
+  } if (typeof a.valueOf() === 'string') {
+    return true;
+  }
+  return false;
 }
 
 
